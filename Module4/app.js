5852d20dc09dc02ab6b6e71a0f435572fc367d70
Aniket Mittal <20BCS6819@cuchd.in>
2022-06-20 15:41:10 +0530

    Update app.js

--- Module4/app.js
+++ Module4/app.js
@@ -1,1 +1,21 @@
 
+
+
+const inputname = document.querySelector(".inputname");
+const output = document.querySelector(".output");
+const btn = document.querySelector(".btn");
+
+btn.addEventListener("click", (e)=>{
+    e.preventDefault();
+})
+
+inputname.addEventListener("change", (e)=>{
+    const value = e.target.value;
+    const greeting = (value[0] === 'j' || value[0] === 'J') ? "Hello" : "Goodbye" ;
+    output.innerHTML = `
+    <p>${greeting} ${value}</p>
+    `;
+})
+
+
+
